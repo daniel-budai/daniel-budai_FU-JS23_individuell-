38df872fef0acaadc1729ab8a595196ac3d1c9dd
daniel-budai <danielbudai@tutanota.com>
2024-03-01 19:11:51 +0100

    third

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
+import "./Landing.scss";
 
 const Landing: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate("/navigation");
+  };
+
   return (
-    <div>
-      <h1>testtest</h1>
+    <div className="landing" onClick={handleNavigation}>
+      <img
+        className="graphic-left"
+        src="./src/assets/graphic-left.svg"
+        alt="left"
+      />
+      <div className="airbean-logo">
+        <img src="./src/assets/airbean-icon.svg" alt="airbean-logo" />
+      </div>
+      <div className="landing-logo">
+        <img src="./src/assets/group 1.svg" alt="airbean-logo" />
+      </div>
+      <img
+        className="graphic-right"
+        src="./src/assets/graphic-right.svg"
+        alt="right"
+      />
     </div>
   );
 };
--- src/pages/Navigation/Navigation.tsx
+++ src/pages/Navigation/Navigation.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Navigation.scss";
 
 const Navigation: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <div className="nav">
+        <ul>
+          <li>Meny</li>
+          <img className="line" src="./src/assets/shortline.svg" alt="" />
+          <li>Vårt kaffe</li>
+          <img className="line" src="./src/assets/shortline.svg" alt="" />
+          <li>Min profil</li>
+          <img className="line" src="./src/assets/shortline.svg" alt="" />
+          <li>Orderstatus</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Navigation;
