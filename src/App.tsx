3ec5e9bfd251349f1fbfdc18da79245980bf57e7
daniel-budai <danielbudai@tutanota.com>
2024-03-01 13:34:22 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import "./sass/_index.scss";
+import Landing from "./pages/Landing/Landing";
+import Navigation from "./pages/Navigation/Navigation";
+import Menu from "./pages/Menu/Menu";
+import About from "./pages/About/About";
+import Status from "./pages/Status/Status";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/navigation" element={<Navigation />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/status" element={<Status />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1>testtest</h1>
+    </div>
+  );
+};
+
+export default Landing;
