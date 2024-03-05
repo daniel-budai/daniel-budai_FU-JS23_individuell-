d80d489c639612cf180b0d59d8d9258110935236
daniel-budai <danielbudai@tutanota.com>
2024-03-05 08:36:20 +0100

    statuspage

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,13 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./HamburgerMenu.scss";
 
 const HamburgerMenu: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/navigation");
+  };
+
   return (
     <div className="hamburger-menu">
       <img
         src="./src/assets/navicon.png"
         alt="Hambuger menu icon"
         className="navicon"
-        //onClick=
+        onClick={handleClick}
       />
     </div>
   );
--- src/pages/Status/Status.tsx
+++ src/pages/Status/Status.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Status.scss";
+import drone from "../../assets/drone.svg";
 
-const Status: React.FC = () => {
-  return <></>;
+const StatusPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/navigation");
+  };
+
+  return (
+    <div className="status-page">
+      <p className="status-ordernumber">
+        Ordernummer: <span>#12DV23F</span>
+      </p>
+      <img src={drone} alt="drone" />
+      <h1 className="order-text">
+        Din beställning <br /> är på väg
+      </h1>
+      <p className="minutes">
+        <span>13</span> minuter
+      </p>
+      <button className="confirm-button" onClick={handleClick}>
+        Ok, cool!
+      </button>
+    </div>
+  );
 };
 
-export default Status;
+export default StatusPage;
